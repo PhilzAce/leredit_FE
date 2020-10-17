05194bd32bb27c678e8f92dd73eb7f8071f80b4c
Philzace1 <unknownumeh@gmail.com>
2020-10-17 14:03:19 +0100

    bug fix

--- src/util/toErrorMap.ts
+++ src/util/toErrorMap.ts
@@ -1,6 +1,7 @@
 import { FieldError } from '../generated/graphql';
 
 export const toErrorMap = (errors: FieldError[]) => {
+  console.log(errors, "there is something we are sure we can do about the errors that we are getting ")
   const errorMap: Record<string, string> = {};
   errors.forEach(({ field, message }) => {
     errorMap[field] = message;
